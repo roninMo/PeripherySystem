61aeb78475e819436c89a8ab613e651703ed9115
Kieran Schwegman <kschwegman1@gmail.com>
2024-07-29 10:18:03 -0400

    Update PeripherySystemComponent.Build.cs

--- Plugins/PeripherySystemComponent/Source/PeripherySystemComponent/PeripherySystemComponent.Build.cs
+++ Plugins/PeripherySystemComponent/Source/PeripherySystemComponent/PeripherySystemComponent.Build.cs
@@ -8,46 +8,18 @@ public class PeripherySystemComponent : ModuleRules
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
 				"CoreUObject",
+				"InputCore",
 				"Engine",
-				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
+				"NetCore",
+				"PhysicsCore",
+				"DataRegistry"
 			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+		);
+
 	}
 }
